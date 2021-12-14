80f2a038ece0ca37b8520492f965f89d3e5bd5bc
N-HIROYASU <nhiro1109pri@gmail.com>
2021-12-14 23:14:20 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "spm-trial",
+            name: "SpmTrial",
             targets: ["spm-trial"]),
     ],
     dependencies: [
