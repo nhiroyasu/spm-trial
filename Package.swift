7cc517a0ccc6361f74e600d0f704f63ebf067749
N-HIROYASU <nhiro1109pri@gmail.com>
2021-12-14 23:16:44 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "spm-trial",
+    name: "SpmTrial",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SpmTrial",
+            name: "spm-trial",
             targets: ["spm-trial"]),
     ],
     dependencies: [
